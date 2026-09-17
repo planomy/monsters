0d5cba3947cfee176493e66bcaf486bb18c89389
planomy <nic@eq.edu.au>
2026-09-18 09:07:03 +1000

    Build Monsterz Play Sky Drop prototype

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { initTheme } from './hooks/useTheme'
-import { initUiScale } from './hooks/useUiScale'
 import './index.css'
 import App from './App.tsx'
 
-initTheme()
-initUiScale()
-
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <StrictMode><App /></StrictMode>,
 )
